9db85453390ee45993780e5fdac444e1348ad832
Michael Stocker <mic.sto@hotmail.com>
2016-12-09 22:07:05 +0100

    Added some fancy commentary

--- src/njPiano.h
+++ src/njPiano.h
@@ -1,3 +1,12 @@
+/* ***************************************************************************
+ * A small library that allows creating 'music pieces' on any modern computer.
+ *
+ * The library was originally written as a for-fun project back in school and
+ * we decided to go over it once again and release it for the world to enjoy.
+ *
+ * Authors: Michael Stocker (dot_Sp0T) & Patrick Bächli (McDonnough)
+ */
+
 #ifndef NJPIANO_H_
 #define NJPIANO_H_
 
@@ -32,21 +41,55 @@ typedef enum Duration {
 	Sechzehntel = Semiquaver
 } Duration;
 
-typedef struct Dynamite {
+typedef struct Piecelet {
 	Note note;
 	Duration duration;
 	int octave;
-} Dynamite;
+} Piecelet;
 
+/* ======================================================================== */
 
-int init(int bpm, bool enableSound);
+/*
+ * Initializes and/or resets the library. MUST be called before using it.
+ *
+ * bmp         -> the desired Beats Per Minute
+ * enableSound -> if the lib should output sounds while creating a music piece
+ */
+int init(int bpm, bool enableSound = false);
+
+/*
+ * Releases allocated resources and cleans up.
+ */
 int kill();
 
-int append(Note, Duration);
-int remove();
-int clear();
-const Dynamite* getDynamites();
+/* ======================================================================== */
 
-int honeyTheyrePlayingOurSong(const Dynamite*);
+/*
+ * Appends a single Note measuring the desired Duration to the music piece
+ */
+int append(Note, Duration);
+
+/*
+ * Removes the last Note that was added to the music piece
+ */
+int remove();
+
+/*
+ * Removes all notes and leaves an empty music piece as if the init method had
+ * been called
+ */
+int clear();
+
+/*
+ * Returns the music piece as of this point in time
+ */
+const Piecelet* getMusicPiece();
+
+/* ======================================================================== */
+
+/*
+ * Plays a music piece
+ */
+int honeyTheyrePlayingOurSong(const Piecelet*);
 
 #endif /* NJPIANO_H_ */
